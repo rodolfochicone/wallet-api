b2ff8a773b7290e36b3b2a12828cbaa85b9b4652
Rodolfo Aparecido Chicone <rodolfo-chicone@rodolfo.chicone>
2020-10-12 13:19:10 -0300

    config testes

---
+++ src/test/java/com/wallet/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.wallet;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class HelloWorld {
+
+    @Test
+    public void testHelloWorld(){
+        Assert.assertEquals(1,1);
+    }
+}
